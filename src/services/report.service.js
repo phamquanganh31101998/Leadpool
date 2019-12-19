e67363d67ea8673487ceeb333ee1ccca2f5555c1
anhpq.adsplus <anhpq.adsplus@gmail.com>
2019-12-19 17:26:55 +0700

    report v3

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -8,39 +8,71 @@ export default {
     getContactRegularlyCare, getDealAmountStaff, getDealAmountStage
 }
 
-function getContactPerMonth(idAccount){
+function getContactPerMonth(idAccount, time){
+    let a = null; 
+    let _qs = null;
+    if (time) {
+        a = {
+            time: time
+        }
+        _qs = qs.stringify(a);
+    }
     let request = {
         method: 'GET',
         headers: authHeader()
     }
-    let endpoint = `${config.apiContact}/${idAccount}/contacts/contactPermonth`
+    let endpoint = (_qs == null) ? `${config.apiContact}/${idAccount}/contacts/contactPermonth` : `${config.apiContact}/${idAccount}/contacts/contactPermonth?${_qs}`
     return responseService.fetchRetry(endpoint, request, 1)
 }
 
-function getContactPerStaff(idAccount){
+function getContactPerStaff(idAccount, time){
+    let a = null; 
+    let _qs = null;
+    if (time) {
+        a = {
+            time: time
+        }
+        _qs = qs.stringify(a);
+    }
     let request = {
         method: 'GET',
         headers: authHeader()
     }
-    let endpoint = `${config.apiContact}/${idAccount}/contacts/contactPerStaff`
+    let endpoint = (_qs == null) ? `${config.apiContact}/${idAccount}/contacts/contactPerStaff` : `${config.apiContact}/${idAccount}/contacts/contactPerStaff?${_qs}`
     return responseService.fetchRetry(endpoint, request, 1)
 }
 
-function getContactPerStaffDetail(idAccount){
+function getContactPerStaffDetail(idAccount, time){
+    let a = null; 
+    let _qs = null;
+    if (time) {
+        a = {
+            time: time
+        }
+        _qs = qs.stringify(a);
+    }
     let request = {
         method: 'GET',
         headers: authHeader()
     }
-    let endpoint = `${config.apiContact}/${idAccount}/contacts/owner-leadstatus`
+    let endpoint = (_qs == null) ? `${config.apiContact}/${idAccount}/contacts/owner-leadstatus` : `${config.apiContact}/${idAccount}/contacts/owner-leadstatus?${_qs}`
     return responseService.fetchRetry(endpoint, request, 1)
 }
 
-function getContactRegularlyCare(idAccount){
+function getContactRegularlyCare(idAccount, time){
+    let a = null; 
+    let _qs = null;
+    if (time) {
+        a = {
+            time: time
+        }
+        _qs = qs.stringify(a);
+    }
     let request = {
         method: 'GET',
         headers: authHeader()
     }
-    let endpoint = `${config.apiContact}/${idAccount}/contacts/regularly-care`
+    let endpoint = (_qs == null) ? `${config.apiContact}/${idAccount}/contacts/regularly-care` :  `${config.apiContact}/${idAccount}/contacts/regularly-care?${_qs}`;
     return responseService.fetchRetry(endpoint, request, 1)
 }
 
@@ -53,11 +85,19 @@ function getDealAmountStaff(idAccount){
     return responseService.fetchRetry(endpoint, request, 1)
 }
 
-function getDealAmountStage(idAccount){
+function getDealAmountStage(idAccount, time){
+    let a = null; 
+    let _qs = null;
+    if (time) {
+        a = {
+            time: time
+        }
+        _qs = qs.stringify(a);
+    }
     let request = {
         method: 'GET',
         headers: authHeader()
     }
-    let endpoint = `${config.apiContact}/${idAccount}/deals/amount-stage`
+    let endpoint = (_qs == null) ? `${config.apiContact}/${idAccount}/deals/amount-stage` : `${config.apiContact}/${idAccount}/deals/amount-stage?${_qs}`
     return responseService.fetchRetry(endpoint, request, 1)
 }
